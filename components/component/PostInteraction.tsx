c6d1eff9bfb4ced05e2c3bf0789bf45972332183
Takahashi <akaneiro.web.design@gmail.com>
2025-03-02 11:24:28 +0900

    セクション4-29 修正前

---
+++ components/component/PostInteraction.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import { HeartIcon, MessageCircleIcon, Share2Icon, ClockIcon } from "./Icons";
+import  { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+type PostInteractionProps = {
+    postId: string;
+    initialLikes: string[];
+    commentNumber: number;
+}
+
+const PostInteraction = ({ 
+    postId,
+    initialLikes,
+    commentNumber,
+}: PostInteractionProps) => {
+    const likeAction = async () => {
+        "use server";
+        console.log(postId);
+
+        const { userId } = auth();
+
+            if(!userId) {
+                throw new Error("User is not authenticated");
+            }
+        try {
+            
+            const existingLike = await prisma.like.findFirst({
+                where: {
+                    postId,
+                    userId,
+                },
+            });
+
+            if (existingLike) {
+                await prisma.like.delete({
+                    where: {
+                        id: existingLike.id,
+                    },
+                });
+
+                revalidatePath("/");
+            } else {
+                await prisma.like.create({
+                    data: {
+                        postId,
+                        userId,
+                    },
+                });
+                revalidatePath("/");
+            }
+            } catch (err) {
+                console.log(err);
+        }
+    };
+
+
+    return (
+        <div className="flex items-center">
+            <form action={likeAction}>
+            <Button variant="ghost" size="icon">
+                <HeartIcon className="h-5 w-5 text-muted-foreground" />
+            </Button>
+            </form>
+        <span className="-ml-1 text-destructive">{initialLikes.length}</span>
+            <Button variant="ghost" size="icon">
+                <MessageCircleIcon className="h-5 w-5 text-muted-foreground" />
+        </Button>
+        <span>{commentNumber}</span>
+            <Button variant="ghost" size="icon">
+                <Share2Icon className="h-5 w-5 text-muted-foreground" />
+        </Button>
+        </div>
+    )
+}
+
+export default PostInteraction;
