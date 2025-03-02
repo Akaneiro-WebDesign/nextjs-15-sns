bee14f65accc93dfd58584a68febfa69beb6a269
Takahashi <akaneiro.web.design@gmail.com>
2025-03-02 13:56:01 +0900

    セクション4-30

--- components/component/PostInteraction.tsx
+++ components/component/PostInteraction.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+"use client";
+
+import React, { FormEvent, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { HeartIcon, MessageCircleIcon, Share2Icon, ClockIcon } from "./Icons";
-import  { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
+import  { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
+import { useAuth } from "@clerk/nextjs";
+import { likeAction } from "@/lib/actions";
 
 type PostInteractionProps = {
     postId: string;
@@ -17,63 +21,38 @@ const PostInteraction = ({
     initialLikes,
     commentNumber,
 }: PostInteractionProps) => {
-    const likeAction = async () => {
-        "use server";
-        console.log(postId);
-    
-        const { userId: clerkUserId } = auth();
-    
-        if (!clerkUserId) {
-            throw new Error("ユーザーが認証されていません");
-        } 
-    
+    const { userId } = useAuth()
+
+    const [likeState, setLikeState] = useState({
+        likeCount: initialLikes.length,
+         isLiked: userId ? initialLikes.includes(userId): false,
+        });
+
+    const handleLikeSubmit = async (e: FormEvent) => {
+        e.preventDefault();
         try {
-            // まずデータベースでユーザーを検索
-            const user = await prisma.user.findUnique({
-                where: {
-                    clerkId: clerkUserId,
-                },
-            });
-            
-            if (!user) {
-                throw new Error("ユーザーがデータベースに見つかりません");
-            }
-            const existingLike = await prisma.like.findFirst({
-                where: {
-                    postId,
-                    userId: user.id, // PrismaのユーザーIDを使用
-                },
-            });
-    
-            if (existingLike) {
-                await prisma.like.delete({
-                    where: {
-                        id: existingLike.id,
-                    },
-                });
-                revalidatePath("/");
-            } else {
-                await prisma.like.create({
-                    data: {
-                        postId,
-                        userId: user.id, // PrismaのユーザーIDを使用
-                    },
-                });
-                revalidatePath("/");
-            }
+            setLikeState((prev) => ({
+                likeCount: prev.isLiked ? prev.likeCount - 1 :prev.likeCount + 1,
+                isLiked: !prev.isLiked,
+            }));
+            await likeAction(postId);
         } catch (err) {
-            console.log(err);
+            setLikeState((prev) => ({
+                likeCount: prev.isLiked ? prev.likeCount + 1 :prev.likeCount - 1,
+                isLiked: !prev.isLiked,
+            }));
+            console.error(err);
         }
     };
 
     return (
         <div className="flex items-center">
-            <form action={likeAction}>
+            <form onSubmit={handleLikeSubmit}>
             <Button variant="ghost" size="icon">
                 <HeartIcon className="h-5 w-5 text-muted-foreground" />
             </Button>
             </form>
-        <span className="-ml-1 text-destructive">{initialLikes.length}</span>
+        <span className="-ml-1 text-destructive">{likeState.likeCount}</span>
             <Button variant="ghost" size="icon">
                 <MessageCircleIcon className="h-5 w-5 text-muted-foreground" />
         </Button>
--- lib/actions.ts
+++ lib/actions.ts
@@ -78,3 +78,52 @@ export async function addPostAction(
     }
   }
 }
+
+export const likeAction = async (postId: string) => {
+  "use server";
+  console.log(postId);
+
+  const { userId: clerkUserId } = auth();
+
+  if (!clerkUserId) {
+      throw new Error("ユーザーが認証されていません");
+  } 
+
+  try {
+      // まずデータベースでユーザーを検索
+      const user = await prisma.user.findUnique({
+          where: {
+              clerkId: clerkUserId,
+          },
+      });
+
+      if (!user) {
+          throw new Error("ユーザーがデータベースに見つかりません");
+      }
+      const existingLike = await prisma.like.findFirst({
+          where: {
+              postId,
+              userId: user.id, // PrismaのユーザーIDを使用
+          },
+      });
+
+      if (existingLike) {
+          await prisma.like.delete({
+              where: {
+                  id: existingLike.id,
+              },
+          });
+          revalidatePath("/");
+      } else {
+          await prisma.like.create({
+              data: {
+                  postId,
+                  userId: user.id, // PrismaのユーザーIDを使用
+              },
+          });
+          revalidatePath("/");
+      }
+  } catch (err) {
+      console.log(err);
+  }
+};
